765020866b663b0f44bba3da5cfef344ad194b69
Boyue-Guo <gby@mail.ustc.edu.cn>
2026-05-05 12:48:15 +0000

    Deploying to gh-pages from @ Boyue-Guo/Boyue-Guo.github.io@8165d683964c0aedd06be34620cd24f12da0ea2d 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Boyue-Guo/123",
+    "data-repo": "Boyue-Guo/Boyue-Guo.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
